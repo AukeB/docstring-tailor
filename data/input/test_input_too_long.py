5787e562a95f28255717f87ce181a674244a0840
AukeB <afbruinsma@gmail.com>
2026-05-30 16:52:03 +0200

    Updated

--- data/input/test_input_too_long.py
+++ data/input/test_input_too_long.py
@@ -17,8 +17,14 @@ class VeryImportantClass:
         
         Returns:
             result (float): Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+        
+        Raises:
+            ValueError: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
         """
         result = float_parameter * 2
+
+        if result > 500:
+            raise ValueError("Not allowed")
 
         return result
         
